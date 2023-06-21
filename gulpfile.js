bf32b80d60575f9b8f3bdb4e41acb90a8b63e32e
Antoine Pineau <a.pineau@eurelis.com>
2023-06-21 21:35:58 +0200

    reorga

--- gulpfile.js
+++ gulpfile.js
@@ -10,43 +10,45 @@ const reload = ()=>{
 }
 // Task to build the HTML files with includes
 gulp.task('html', () => {
-  return gulp.src('src/html/*.html')
+  return gulp.src(['src/html/**/*.html', '!src/html/includes/*.html'])
     .pipe(htmlExtend({annotations:true, verbose:true}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest((file)=>{
+      return file.base.replace('src/html', 'dist')
+    }))
     .pipe(browserSync.stream());
 });
 
 // Task to build the SCSS files into concatenated and minified CSS
 gulp.task('scss', () => {
-  return gulp.src('src/scss/main.scss')
+  return gulp.src('src/res/scss/main.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(concat('main.min.css'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(gulp.dest('dist/res/css'))
+    .pipe(browserSync.stream());
 });
 
 // Task to build and minify JavaScript files
 gulp.task('js', () => {
-  return gulp.src(['src/js/lib/mavo.js', 'src/js/lib/mavo-locale-fr.js', 'src/js/main.js'])
+  return gulp.src(['src/res/js/lib/mavo.js', 'src/res/js/lib/mavo-locale-fr.js', 'src/res/js/main.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('dist/res/js'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('copy-css-map', () => {
-  return gulp.src('src/scss/lib/maps/mavo.css.map')
-    .pipe(gulp.dest('dist/css/lib/maps'));
+  return gulp.src('src/res/scss/lib/maps/mavo.css.map')
+    .pipe(gulp.dest('dist/res/css/lib/maps'));
 })
 
 gulp.task('copy-js-map', () => {
-  return gulp.src('src/js/lib/maps/mavo.js.map')
-    .pipe(gulp.dest('dist/js/lib/maps'));
+  return gulp.src('src/res/js/lib/maps/mavo.js.map')
+    .pipe(gulp.dest('dist/res/js/lib/maps'));
 })
 
 gulp.task('copy-img', () => {
-  return gulp.src('src/img/*')
-    .pipe(gulp.dest('dist/img/'));
+  return gulp.src('src/res/img/*')
+    .pipe(gulp.dest('dist/res/img/'));
 })
 
 gulp.task('copy-files', gulp.series(['copy-css-map', 'copy-js-map', 'copy-img']));
@@ -65,8 +67,8 @@ gulp.task('serve', () => {
   });
  
   gulp.watch('src/html/**/*.html', gulp.series('html')).on('change', reload);
-  gulp.watch('src/scss/**/*.scss', gulp.series('scss')).on('change', reload);
-  gulp.watch('src/js/**/*.js', gulp.series('js')).on('change', reload);
+  gulp.watch('src/res/scss/**/*.scss', gulp.series('scss')).on('change', reload);
+  gulp.watch('src/res/js/**/*.js', gulp.series('js')).on('change', reload);
   gulp.watch('gulpfile.js').on('change', reload);
 });
 
