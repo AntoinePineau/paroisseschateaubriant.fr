e46629df95297473c0f2c3f40d4f027c9eac5ffb
Antoine Pineau <a.pineau@eurelis.com>
2023-07-10 00:09:41 +0200

    update template, home and actualites

--- src/res/js/main.js
+++ src/res/js/main.js
@@ -1,1 +1,14 @@
 /* custom code */
+function addMenuInteraction() {
+    var navButton = document.querySelector('nav button');
+    navButton.addEventListener('click', function() {
+        let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+        this.setAttribute('aria-expanded', !expanded);
+        let menu = this.nextElementSibling;
+        navButton.querySelector('span').innerText = expanded ? '☰' : '✕';
+    });
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    addMenuInteraction();
+})
