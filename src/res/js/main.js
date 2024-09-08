29699693b2be9e28e0158c3733fd874b81af9351
Antoine Pineau <a.pineau@eurelis.com>
2024-09-08 22:59:55 +0200

    update 28/09

--- src/res/js/main.js
+++ src/res/js/main.js
@@ -1,28 +1,11 @@
 document.addEventListener('DOMContentLoaded',()=>{
   
-    if(document.querySelector('main[mv-app=chants]')) {
+    if(document.querySelector('main[id=chants]')) {
       initChants();
     }
 
-    if(document.querySelector('main[mv-app=bible]')) {
+    if(document.querySelector('main[id=bible]')) {
       initBible();
     }
 
-    if(document.querySelector('main[mv-app=recherche]'))
-      fullSearch(document.location.href.split('texte=')[1]);
-
-    Mavo.inited
-    //.then(() => Mavo.all[0].dataLoaded)
-    .then(()=>{
-      setTimeout(()=>{
-        loadContacts();
-      }, 500)
-      return Mavo.all[0].dataLoaded
-    })
-    .then(()=>{
-        setTimeout(()=>{
-          loadContacts();
-          addContactsSearch();
-        }, 1000)
-      });
 })
